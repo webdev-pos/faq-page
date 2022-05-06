5be3ff44b7c8c9ac3650b097fd8159e013268900
NichlasBach <105077694+NichlasBach@users.noreply.github.com>
2022-05-06 20:52:37 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const question = document.getElementsByClassName("button-question");
+
+for (let i = 0; i < question.length; i++) {
+  question[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
